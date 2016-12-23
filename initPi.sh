b79ad3e9abbaa182247a8d4de039b0d5b3d7a785
dbaCindy <cindy@dbacindy.com>
2016-12-23 04:35:05 +0000

    Added Static IPs

--- initPi.sh
+++ initPi.sh
@@ -1,17 +1,11 @@
 
 export v_date=`date +"%y%m%d_%H%M%S"`
-#apt-get -y update
+apt-get -y update
 cp /etc/default/keyboard /etc/default/keyboard.$v_date
 cp ./initPi/etc/default/keyboard /etc/default/keyboard
 
 ./initGit.sh
 ./fetchGit.sh
 
-### Build wlan0 as AP
-cd ./buildAP
-./buildAP.sh
-cd ..
-
-shutdown -r now
 
 
